e5ee41813887b70e38026da72c55e8e70cf92dcb
David Ren <davidren0423@icloud.com>
2024-10-03 20:48:40 -0400

    idk

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from spotipy.oauth2 import SpotifyOAuth
 scope = "user-library-read"
 SPOTIPY_CLIENT_ID = "f0a3099ca9a84a3fb39187237750d4ed"
 SPOTIPY_CLIENT_SECRET = "cdcbd1ef8031430ba7cf655c4813c76d"
-SPOTIPY_REDIRECT_URI='https://www.google.com/'
+SPOTIPY_REDIRECT_URI = 'https://www.google.com/'
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri=SPOTIPY_REDIRECT_URI,
+    scope=scope
+))
 
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
